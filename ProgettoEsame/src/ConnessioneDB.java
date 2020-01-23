52557c864fce62e6426d5895401ba2f917a3a38e
Kenzo988 <Kenzo988@users.noreply.github.com>
2020-01-23 18:52:34 +0100

    Modifiche al controller

---
+++ ProgettoEsame/src/ConnessioneDB.java
@@ -0,0 +1,148 @@
+import java.util.*;
+
+import java.sql.*;
+import java.sql.Date;
+
+public class ConnessioneDB 
+{
+	public Connection CreaConnessione(String utente, String password, Connection con) 
+	{
+
+		//utente default
+		if(utente.equals(""))
+			utente = "postgres";
+		if(password.equals(""))
+			password = "password";
+		
+		try
+		{
+			//prendo i driver
+			Class.forName("org.postgresql.Driver");
+			System.out.println("driver trovato");
+		}
+		catch(ClassNotFoundException e) 
+		{
+			System.out.println("non trovo il driver");
+		}
+		
+		String url = "jdbc:postgresql://localhost/ProgettoEsame";
+		Properties props = new Properties();
+		props.setProperty("user", utente);
+		props.setProperty("password", password);
+		
+		try 
+		{
+			//creazione connessione
+			con = DriverManager.getConnection(url, props);
+			System.out.println("connesso");
+			return con;
+		}
+		catch(SQLException e) 
+		{
+			System.err.println("connessione fallita");
+			return null;
+		}
+	}
+	
+	public void InserisciAlbum(String nome_album, String nome_artista, Connection con) 
+	{
+		try 
+		{
+			String query;
+			query = "SELECT FROM album_table WHERE nome_album = '"+ nome_album +"' AND artista = '" + nome_artista +"'";
+			PreparedStatement s;
+			s = con.prepareStatement(query);
+			ResultSet rs = s.executeQuery();
+			
+			if(!rs.next()) 
+			{
+				// inserimento album
+				query = "INSERT INTO album_table " + String.format("VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+
+				s = con.prepareStatement(query);
+				s.setString(1, "ep");// tipo album
+				s.setString(2, nome_album);// nome album
+				s.setString(3, nome_artista);// nome artista
+				s.setString(4, "bronzo");// livello artista
+				s.setInt(5, 15);// numero follower
+				s.setInt(6, 125);// views totali
+				s.setDate(7, Date.valueOf("2000-01-01"));// data pubblicazione
+				s.setFloat(8, 15.55f);// retribuzione
+				s.executeUpdate();
+
+				s.close();
+				System.out.println("album inserito");
+			}
+			else
+				System.out.println("album già esistente");
+		}
+		catch(SQLException e) 
+		{
+			System.err.println("inserimento album fallito");
+		}
+	}
+	
+	public void InserisciTraccia(int traccia, String nome_album, String nome_artista, Connection con)
+	{
+		try
+		{
+			String nome_table = "table_" + nome_album;//, check;
+			//check = String.format("SELECT FROM ProgettoEsame  WHERE relname = '?');");
+			PreparedStatement s;
+			//s = con.prepareStatement(check);
+			//s.setString(1, nome_table);
+			//s.executeUpdate();
+			DatabaseMetaData metadata = con.getMetaData();
+			ResultSet rs = metadata.getTables(null, null, nome_table, null);
+			if(!rs.next()) 
+			{
+				try
+				{
+					//creazione subtabella
+					String query = "CREATE TABLE " + nome_table + " (traccia integer, album text, artista text)";
+					s = con.prepareStatement(query);
+					s.executeUpdate();
+					// aggiunta pk
+					/*String nome_pk = nome_table + "_pk";
+					query = "ALTER TABLE " + nome_table + " ADD CONSTRAINT " + nome_pk + " PRIMARY KEY(traccia)";
+					s = con.prepareStatement(query);
+					s.executeUpdate();*/
+					// aggiunta fk
+					String nome_fk = nome_table + "_fk";
+					query = "ALTER TABLE " + nome_table + " ADD CONSTRAINT " + nome_fk
+							+ " FOREIGN KEY(album, artista) REFERENCES album_table (nome_album, artista)";
+					s = con.prepareStatement(query);
+					s.executeUpdate();
+				
+					System.out.println("tabella creata");
+				} 
+				catch (SQLException e) 
+				{
+					System.err.println("creazione tabella fallito");
+				}
+			}
+			// inserimento traccia
+			try 
+			{
+				String query = "INSERT INTO " + nome_table + String.format(" VALUES (?, ?, ?)");
+
+				s = con.prepareStatement(query);
+				s.setInt(1, traccia);// numero traccia
+				s.setString(2, nome_album);// nome album
+				s.setString(3, nome_artista);// nome artista
+				s.executeUpdate();
+
+				s.close();
+				System.out.println("traccia inserita");
+			} 
+			catch (SQLException e) 
+			{
+				System.err.println("inserimento fallito");
+			}
+		}
+		catch(SQLException e) 
+		{
+			System.err.println("errore sql");
+		}
+	}
+}
--- ProgettoEsame/src/Controller.java
+++ ProgettoEsame/src/Controller.java
@@ -1,150 +1,46 @@
 import java.util.*;
 
 import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.Date;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
-import java.sql.Statement;
 
-public class Controller {
+public class Controller 
+{
+	public static Connection con = null;
 	
-	Connection con = null;
-	
-	public boolean CreaConnessione(String utente, String password) 
+	public static void main(String[] args) 
 	{
-		try
+		Controller _mycontroller = new Controller();
+		ConnessioneDB _myconnessione = new ConnessioneDB();
+		//boolean connesso = false;
+		//--------------------------------------
+		do 
 		{
-			//prendo i driver
-			Class.forName("org.postgresql.Driver");
-			System.out.println("Driver trovato");
-		}
-		catch(ClassNotFoundException e) 
-		{
-			System.out.println("Non trovo il driver");
-		}
+		//login
+		String utente, password;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("---login");
 		
-		String url = "jdbc:postgresql://localhost/ProgettoEsame";
-		Properties props = new Properties();
-		props.setProperty("user", utente);
-		props.setProperty("password", password);
+		System.out.println("inserisci utente:");
+		utente = scanner.nextLine();
 		
-		try 
-		{
-			//creazione connessione
-			con = DriverManager.getConnection(url, props);
-			System.out.println("Connesso");
-			return true;
+		System.out.println("inserisci password:");
+		password = scanner.nextLine();
+		
+		//nuova connessione
+		//connesso = _mycontroller.CreaConnessione(utente, password);
+		con = _myconnessione.CreaConnessione(utente, password, con);
+		scanner.close();
 		}
-		catch(SQLException e) 
-		{
-			System.err.println("Connessione fallita");
-			return false;
-		}
+		while(con == null);
+		//---------------------------------------
+		_myconnessione.InserisciAlbum("a", "b", con);
+		_myconnessione.InserisciTraccia(1, "a", "b", con);
+		
+		_myconnessione.InserisciAlbum("b", "b", con);
+		_myconnessione.InserisciTraccia(1, "b", "b", con);
 	}
 	
-	public void InserisciAlbum(String nome_album, String nome_artista) 
+	public Controller ()
 	{
-		try 
-		{
-			String query;
-			query = "SELECT FROM album_table WHERE nome_album = '"+ nome_album +"' AND artista = '" + nome_artista +"'";
-			PreparedStatement s;
-			s = con.prepareStatement(query);
-			ResultSet rs = s.executeQuery();
-			
-			if(!rs.next()) 
-			{
-				// inserimento album
-				query = "INSERT INTO album_table " + String.format("VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
-
-				s = con.prepareStatement(query);
-				s.setString(1, "ep");// tipo album
-				s.setString(2, nome_album);// nome album
-				s.setString(3, nome_artista);// nome artista
-				s.setString(4, "bronzo");// livello artista
-				s.setInt(5, 15);// numero follower
-				s.setInt(6, 125);// views totali
-				s.setDate(7, Date.valueOf("2000-01-01"));// data pubblicazione
-				s.setFloat(8, 15.55f);// retribuzione
-				s.executeUpdate();
-
-				s.close();
-				System.out.println("album inserito");
-			}
-			else
-				System.out.println("album già esistente");
-		}
-		catch(SQLException e) 
-		{
-			System.err.println("inserimento album fallito");
-		}
-	}
-	
-	public void InserisciTraccia(int traccia, String nome_album, String nome_artista)
-	{
-		try
-		{
-			String nome_table = "table_" + nome_album;//, check;
-			//check = String.format("SELECT FROM ProgettoEsame  WHERE relname = '?');");
-			PreparedStatement s;
-			//s = con.prepareStatement(check);
-			//s.setString(1, nome_table);
-			//s.executeUpdate();
-			DatabaseMetaData metadata = con.getMetaData();
-			ResultSet rs = metadata.getTables(null, null, nome_table, null);
-			if(!rs.next()) 
-			{
-				try
-				{
-					//creazione subtabella
-					String query = "CREATE TABLE " + nome_table + " (traccia integer, album text, artista text)";
-					s = con.prepareStatement(query);
-					s.executeUpdate();
-					// aggiunta pk
-					/*String nome_pk = nome_table + "_pk";
-					query = "ALTER TABLE " + nome_table + " ADD CONSTRAINT " + nome_pk + " PRIMARY KEY(traccia)";
-					s = con.prepareStatement(query);
-					s.executeUpdate();*/
-					// aggiunta fk
-					String nome_fk = nome_table + "_fk";
-					query = "ALTER TABLE " + nome_table + " ADD CONSTRAINT " + nome_fk
-							+ " FOREIGN KEY(album, artista) REFERENCES album_table (nome_album, artista)";
-					s = con.prepareStatement(query);
-					s.executeUpdate();
-				
-					System.out.println("tabella creata");
-				} 
-				catch (SQLException e) 
-				{
-					System.err.println("creazione tabella fallito");
-				}
-			}
-			// inserimento traccia
-			try 
-			{
-				String query = "INSERT INTO " + nome_table + String.format(" VALUES (?, ?, ?)");
-
-				s = con.prepareStatement(query);
-				s.setInt(1, traccia);// numero traccia
-				s.setString(2, nome_album);// nome album
-				s.setString(3, nome_artista);// nome artista
-				s.executeUpdate();
-
-				s.close();
-				System.out.println("traccia inserita");
-			} 
-			catch (SQLException e) 
-			{
-				System.err.println("inserimento fallito");
-			}
-		}
-		catch(SQLException e) 
-		{
-			System.err.println("errore sql");
-		}
+		
 	}
 }
