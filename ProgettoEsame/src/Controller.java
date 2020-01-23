c2c75b483b6c8b7b20b9049ff3dbf76e48bdcccd
Kenzo988 <Kenzo988@users.noreply.github.com>
2020-01-23 11:28:47 +0100

    Add main

--- ProgettoEsame/src/Controller.java
+++ ProgettoEsame/src/Controller.java
@@ -10,55 +10,13 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Controller {
+	
+	Connection con = null;
 
-	public static void main(String[] args) 
+	/*public static void main(String[] args) 
 	{
-		Connection con = null;
 		
-		try
-		{
-			//prendo i driver
-			Class.forName("org.postgresql.Driver");
-			System.out.println("Driver trovato");
-		}
-		catch(ClassNotFoundException e) 
-		{
-			System.out.println("Non trovo il driver");
-		}
 		
-		String url = "jdbc:postgresql://localhost/ProgettoEsame";
-		Properties props = new Properties();
-		props.setProperty("user","postgres");
-		props.setProperty("password","password");
-		
-		try 
-		{
-			//creazione connessione
-			con = DriverManager.getConnection(url, props);
-			System.out.println("Connesso");
-		}
-		catch(SQLException e) 
-		{
-			System.err.println("Connessione fallita");
-		}
-		
-		try 
-		{
-			//inserimento riga
-			String query = "INSERT INTO album_table " + String.format("VALUES ('%s', '%s', '%s', '%s', %d, %d, ?, ?)", "album","Album1", "Artista1", "bronzo", 15, 124);
-			
-			PreparedStatement s = con.prepareStatement(query);
-			s.setDate(1, Date.valueOf("2000-01-01"));
-			s.setFloat(2, 15.55f);
-			s.executeUpdate();
-			
-			s.close();
-			System.out.println("Inserito");
-		}
-		catch(SQLException e) 
-		{
-			System.err.println("Inserimento fallito");
-		}
 		
 		try
 		{
@@ -82,6 +40,65 @@ public class Controller {
 		{
 			System.err.println("Creazione table fallito");
 		}
+	}*/
+	
+	public boolean CreaConnessione(String utente, String password) 
+	{
+		try
+		{
+			//prendo i driver
+			Class.forName("org.postgresql.Driver");
+			System.out.println("Driver trovato");
+		}
+		catch(ClassNotFoundException e) 
+		{
+			System.out.println("Non trovo il driver");
+		}
+		
+		String url = "jdbc:postgresql://localhost/ProgettoEsame";
+		Properties props = new Properties();
+		props.setProperty("user", utente);
+		props.setProperty("password", password);
+		
+		try 
+		{
+			//creazione connessione
+			con = DriverManager.getConnection(url, props);
+			System.out.println("Connesso");
+			return true;
+		}
+		catch(SQLException e) 
+		{
+			System.err.println("Connessione fallita");
+			return false;
+		}
 	}
-
+	
+	public void InserisciAlbum(String nome_album, String nome_artista) 
+	{
+		try 
+		{
+			//inserimento riga
+			String query = "INSERT INTO album_table " + String.format("VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+			
+			PreparedStatement s = con.prepareStatement(query);
+			s.setString(1, "ep");//tipo album
+			s.setString(2, nome_album);//nome album
+			s.setString(3, nome_artista);//nome artista
+			s.setString(4, "bronzo");//livello artista
+			s.setInt(5, 15);//numero follower
+			s.setInt(6, 125);//views totali
+			s.setDate(7, Date.valueOf("2000-01-01"));//data pubblicazione
+			s.setFloat(8, 15.55f);//retribuzione
+			s.executeUpdate();
+			
+			s.close();
+			System.out.println("Inserito");
+		}
+		catch(SQLException e) 
+		{
+			System.err.println("Inserimento fallito");
+		}
+		
+	}
 }
---
+++ ProgettoEsame/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		Controller _mycontroller = new Controller();
+		boolean connesso = false;
+		//--------------------------------------
+		do {
+		//login
+		String utente, password;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("---login");
+		
+		System.out.println("inserisci utente:");
+		utente = scanner.nextLine();
+		
+		System.out.println("inserisci password:");
+		password = scanner.nextLine();
+
+		//utente default
+		if(utente.equals(""))
+			utente = "postgres";
+		if(password.equals(""))
+			password = "password";
+		
+		//nuova connessione
+		connesso = _mycontroller.CreaConnessione(utente, password);
+		}
+		while(!connesso);
+		//--------------------------------------
+		
+	}
+
+}
